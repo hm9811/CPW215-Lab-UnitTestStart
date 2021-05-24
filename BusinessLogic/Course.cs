13c0946a2d3fb98b152bc9bcacb5a383fee961e5
hm9811 <hm981198@gmail.com>
2021-05-24 15:30:03 -0700

    Without changing the method signatures; Code the course class so the CourseTests unit tests pass

--- BusinessLogic/Course.cs
+++ BusinessLogic/Course.cs
@@ -6,25 +6,74 @@ namespace BusinessLogic
 {
     public class Course
     {
+        private string _courseName;
+        private string _instructorName;
+        private byte _numberOfCredit;
+
         public Course(string courseName)
         {
+            InstructorName = "STAFF";
             CourseName = courseName;
         }
 
         /// <summary>
         /// The instructor teaching the course
         /// </summary>
-        public string InstructorName { get; set; }
+        public string InstructorName 
+        {
+            get 
+            {
+                return _instructorName;
+            }
+            set
+            {
+                _instructorName = value;
+            }
+        }
 
         /// <summary>
         /// The name of the course
         /// </summary>
-        public string CourseName { get; set; }
+        public string CourseName
+        {
+            get
+            {
+                return _courseName;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    
+                    throw new ArgumentNullException();
+                }
+                else
+                {
+                    _courseName = value;
+                }
+            }
+        }
 
         /// <summary>
         /// Number of credits for the course
         /// </summary>
-        public byte NumberOfCredits { get; set; }
-
+        public byte NumberOfCredits
+        {
+            get
+            {
+                return _numberOfCredit;
+            }
+            set
+            {
+                if (value < 30 && value > 0)
+                {
+                    _numberOfCredit = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
     }
 }
